401af6800a1678f3f9c277b90f446b99e096c309
karlavargas <karlista.ta@gmail.com>
2022-07-08 10:56:29 +0100

    agregue una estructura de control de flujo en el programa de java

--- Prueba.java
+++ Prueba.java
@@ -2,5 +2,9 @@ public class Prueba{
 
 public static void main(String[] args){
   System.out.println("Hola mundo en Java");
+  int a = 34;
+  for(i = 0; i <= a; i++){
+	System.out.println(i);
+	}
 }
 }
